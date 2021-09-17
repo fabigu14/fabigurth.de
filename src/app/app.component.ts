4c32cafe42c8d537e0ccba1a723958e3885b97d1
Fabian Gurth <fabigurth@googlemail.com>
2021-09-17 22:16:56 +0200

    implement slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fabigurth';
+export class AppComponent implements OnInit{
+  images = ['code.jpg', 'workspace.jpg', 'writing.jpg'];
+  currenImg = 0;
+  showImg = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+     setInterval(() => {
+      this.currenImg++;
+      this.currenImg = this.currenImg % this.images.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 10);
+     }, 7000);
+  }
 }
